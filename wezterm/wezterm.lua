8f050fa23f23a27d7dccb143d39a85279798f918
zephyr <i@zephyr.moe>
2024-03-27 23:55:38 +0900

    wezterm: fix conflicts with helix shortcuts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -97,26 +97,36 @@ config.keys = {
   },
   {
     key = 'Enter',
+    mods = 'ALT',
+    action = act.SendKey({ key = 'Enter', mods = 'ALT' })
+  },
+  {
+    key = 'Enter',
+    mods = 'LEADER|ALT',
+    action = 'ToggleFullScreen'
+  },
+  {
+    key = 'Enter',
     mods = 'LEADER',
     action = act_cb(function(win, _)
       local id = tostring(win:window_id())
       local dim = win:get_dimensions()
       if dim.is_full_screen then return end
 
-      local state = term.GLOBAL.toogle_window_state or {}
+      local state = term.GLOBAL.toggle_window_state or {}
       state[id] = state[id] or {
-        toogled = false,
+        toggled = false,
         w = dim.pixel_width,
         h = dim.pixel_height,
       }
       local new_w, new_h
-      if state[id].toogled then
+      if state[id].toggled then
         new_w, new_h = state[id].w, state[id].h
       else
         new_w, new_h = 1200, 850
       end
-      state[id].toogled = not state[id].toogled
-      term.GLOBAL.toogle_window_state = state
+      state[id].toggled = not state[id].toggled
+      term.GLOBAL.toggle_window_state = state
       win:set_inner_size(new_w, new_h)
     end)
   },
