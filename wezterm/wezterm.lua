c85d0a4764eea0263d479eb6a3b3bfd5f59e77db
zephyr <i@zephyr.moe>
2024-05-22 06:35:51 +0900

    wezterm: beautify title bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,12 +4,14 @@ local term = require('wezterm')
 local config = {
   term = 'wezterm',
   front_end = "WebGpu",
+  enable_wayland = true;
   use_ime = true,
   use_dead_keys = false,
-  use_fancy_tab_bar = true,
+  use_fancy_tab_bar = false,
   tab_max_width = 64,
   tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
   cursor_blink_rate = 0,
   status_update_interval = 20000,
   font = term.font_with_fallback({
@@ -50,4 +52,23 @@ config.leader = {
 }
 config.keys = require('keybinds')
 
+-- events --
+term.on('format-tab-title', function(tab)
+  local idx = tab.tab_index
+  local exec = tab.active_pane.foreground_process_name
+  local title = string.gsub(exec, '(.*[/\\])(.*)', '%2')
+  return {
+    { Text = string.format(" %d: %s ", idx + 1, title) }
+  }
+end)
+
+term.on('format-window-title', function(tab, _, tabs)
+  local pane = tab.active_pane
+
+  local zoomed = pane.is_zoomed and '[Z]' or ''
+  local index = tab.tab_index + 1
+  local dir = pane.current_working_dir.file_path
+
+  return string.format("%s [%d/%d] ~ %s", zoomed, index, #tabs, dir)
+end)
 return config
